7846f9bcee91894d6b07fc680175e6b9e132279f
maya-flamby <maylis.boitet@gmail.com>
2025-03-21 11:55:20 +0400

    fix rtd errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,10 @@ html_css_files = [
 ]
 
 
+html_theme = "sphinx_rtd_theme"
+
+html_logo = "graphic/NYU_Logo.png"
+
 html_theme_options = {
     "logo_only": False,
     "display_version": True,
@@ -76,9 +80,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
 
-html_logo = "graphic/NYU_Logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
